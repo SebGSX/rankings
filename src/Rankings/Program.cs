d3bd0ba9fbac85ebbaec2fd26641f6a5cf50d4c6
Seb <127726983+SebGSX@users.noreply.github.com>
2025-10-07 19:01:23 +0200

    feat: Add help system.

--- src/Rankings/Program.cs
+++ src/Rankings/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine();
+﻿// Copyright © 2025 Seb Garrioch. All rights reserved.
+// Published under the MIT License.
+
+using System.CommandLine;
+using Rankings.Resources;
+
+namespace Rankings;
+
+/// <summary>
+///     Represents the application.
+/// </summary>
+public abstract class Program
+{
+    /// <summary>
+    ///     The main entry point for the application.
+    /// </summary>
+    /// <param name="args">The command-line arguments.</param>
+    public static void Main(string[] args)
+    {
+        var rootCommand = new RootCommand(Common.RootCommand_Description);
+        rootCommand.Parse(args).Invoke();
+    }
+}
---
+++ test/Rankings.UnitTests/ProgramTests.cs
@@ -0,0 +1,67 @@
+﻿// Copyright © 2025 Seb Garrioch. All rights reserved.
+// Published under the MIT License.
+
+namespace Rankings.UnitTests;
+
+/// <summary>
+///     Unit tests for the <see cref="Program"/> class.
+/// </summary>
+public class ProgramTests
+{
+    /// <summary>
+    ///     Tests that the <see cref="Program.Main"/> method displays help when called with the "--help" argument.
+    /// </summary>
+    [Theory]
+    [InlineData("-?")]
+    [InlineData("-h")]
+    [InlineData("--help")]
+    public void Main_WithHelpArgument_DisplaysHelp(string arg)
+    {
+        // Arrange
+        const string expected = "Show help and usage information";
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
+
+        // Act
+        Program.Main([arg]);
+        var actual = sw.ToString();
+
+        // Assert
+        Assert.Contains(expected, actual);
+    }
+    
+    /// <summary>
+    ///     Tests that the <see cref="Program.Main"/> method displays an error message when called with an invalid
+    ///     argument.
+    /// </summary>
+    [Fact]
+    public void Main_WithInvalidArgument_DisplaysError()
+    {
+        // Arrange
+        const string expected = "Unrecognized command or argument '--invalid'.";
+        const string invalidArg = "--invalid";
+        using var sw = new StringWriter();
+        Console.SetError(sw);
+
+        // Act
+        Program.Main([invalidArg]);
+        var actual = sw.ToString();
+
+        // Assert
+        Assert.Contains(expected, actual);
+    }
+    
+    /// <summary>
+    ///     Tests that the <see cref="Program.Main"/> method does not throw an exception when called with no arguments.
+    /// </summary>
+    [Fact]
+    public void Main_WithNoArguments_DoesNotThrow()
+    {
+        // Arrange
+        var args = Array.Empty<string>();
+
+        // Act & Assert
+        var exception = Record.Exception(() => Program.Main(args));
+        Assert.Null(exception);
+    }
+}
